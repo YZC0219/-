1ac33de8edd2eaabd27f49e32692a9d4fb826172
YZC0219 <3207440983@qq.com>
2026-09-18 22:07:27 +0800

    Add problem statement summary to P1001

--- luogu/P1001_APlusB.cpp
+++ luogu/P1001_APlusB.cpp
@@ -1,5 +1,10 @@
-// 题号：P1001
-// 题目：A+B Problem
+/*
+题号：P1001
+题目：A+B Problem
+题干：输入两个整数 a、b，计算并输出它们的和。
+输入：两个整数 a、b。输出：a+b。
+原题：https://www.luogu.com.cn/problem/P1001
+*/
 
 #include <iostream>
 using namespace std;
