e10cdc8977af550a2598d7816fb96b5874ac13ad
YZC0219 <3207440983@qq.com>
2026-09-18 22:08:57 +0800

    Add problem statement summary to P5719

--- luogu/P5719_Category_Average.cpp
+++ luogu/P5719_Category_Average.cpp
@@ -1,5 +1,10 @@
-// 题号：P5719
-// 题目：分类平均
+/*
+题号：P5719
+题目：分类平均
+题干：把 1 到 n 的正整数分成两类：能被 k 整除的 A 类和不能被 k 整除的 B 类，分别求两类数的平均值。
+输入：正整数 n、k。输出：A、B 两类平均数，均保留 1 位小数。
+原题：https://www.luogu.com.cn/problem/P5719
+*/
 
 #include <iostream>
 #include <iomanip>
