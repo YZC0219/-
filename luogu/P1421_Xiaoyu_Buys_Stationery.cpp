215482199d6975574213c865b96753bc6aaef12a
YZC0219 <3207440983@qq.com>
2026-09-18 22:08:17 +0800

    Add problem statement summary to P1421

--- luogu/P1421_Xiaoyu_Buys_Stationery.cpp
+++ luogu/P1421_Xiaoyu_Buys_Stationery.cpp
@@ -1,5 +1,10 @@
-// 题号：P1421
-// 题目：小玉买文具
+/*
+题号：P1421
+题目：小玉买文具
+题干：小玉有 a 元 b 角，每支签字笔价格为 1 元 9 角，求最多能买多少支。
+输入：两个整数 a、b。输出：最多能购买的签字笔数量。
+原题：https://www.luogu.com.cn/problem/P1421
+*/
 
 #include <iostream>
 using namespace std;
